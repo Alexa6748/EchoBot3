e2a0596f0798cd3e1d956e5801f88cb14fdd73d1
Alexa6748 <sandjieva.alex@gmail.com>
2022-03-18 16:20:09 +0300

    Добавьте файлы проекта.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.15.2
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace EchoBot3.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        private readonly string[] _cards =
+       {
+            Path.Combine(".", "Resources", "Time.json")
+        };
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            //var replyText = $"Echo: {turnContext.Activity.Text}";
+            //await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            if (string.Equals(turnContext.Activity.Text, "время", System.StringComparison.InvariantCultureIgnoreCase))
+            {
+                DateTime date1 = DateTime.Now;
+                var cardAttachment = CreateAdaptiveCardAttachment(Path.Combine(".", "Resources", "Time.json"));
+                
+                //turnContext.Activity.Attachments = new List<Attachment>() { cardAttachment };
+                await turnContext.SendActivityAsync(MessageFactory.Attachment(cardAttachment), cancellationToken);
+            }
+            else
+            {
+                var replyText = $"Echo: {turnContext.Activity.Text}";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+        private static Attachment CreateAdaptiveCardAttachment(string filePath)
+        {
+            var adaptiveCardJson = File.ReadAllText(filePath);
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+    }
+}
